b28e146730fd5896291d957a95d73f4d7dc2f4a4
retoc <retoc@qq.com>
2022-03-17 13:14:59 +0800

    Test1

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    //我是一段注释    我是一段注释   我是一段注释  我是一段注释   我是一段注释    我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释   我是一段注释
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
